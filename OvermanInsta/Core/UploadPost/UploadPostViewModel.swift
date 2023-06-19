1e5c66d452c702fa0f0cf6c471607e45cbf3bc22
WaterMan-Joy <nietzsche21st@gmail.com>
2023-06-19 20:28:29 +0900

    add package firebase auth, firebase firestore, firebaseStorage

--- OvermanInsta/Core/UploadPost/UploadPostViewModel.swift
+++ OvermanInsta/Core/UploadPost/UploadPostViewModel.swift
@@ -15,6 +15,7 @@ class UploadPostViewModel: ObservableObject {
     @Published var selectedImage: PhotosPickerItem? {
         didSet { Task { await loadImage(fromItem: selectedImage)}}
     }
+    
     @Published var postImage: Image?
     
     func loadImage(fromItem item: PhotosPickerItem?) async {
